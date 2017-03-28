c682b1379f4ff6e7f87a6c8bf8d98fa74ceeab68
BhumiSukhadiya <lanetteam.bhumika@gmail.com>
2017-03-28 17:29:58 +0530

    Add files via upload

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -1,40 +1,32 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+import {connect} from 'react-redux';
 // Import Style
 import styles from './Header.css';
 
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { check: false };
-  }
-  componentDidMount() {
-    if (this.checkAuth()) {
-      this.setState({ check: true });
+    constructor(props) {
+        super(props);
+        this.state = {check: false};
     }
-    console.log(this.state.check);
-  }
-  checkAuth = () => {
-    return localStorage.getItem('token') !== null;
-  }
-  render() {
-    return (
-      <div className={styles.header}>
-        <div className={styles.content}>
-          <h1 className={styles['site-title']}>
-            <Link to="/">MERN STARTER BLOG</Link>
-          </h1>
-          <div>
-            {!this.state.check && <p><Link to="/login" className={styles['add-post-button']}>Log In</Link>
-              <Link to="/register" className={styles['add-post-button']}>Register Here</Link></p>}
-            <Link to="/" className={styles['add-post-button']}>Home</Link>
-          </div>
-        </div>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className={styles.header}>
+              <div className={styles.content}>
+                <h1 className={styles['site-title']}>
+                  <Link to="/">MERN STARTER BLOG</Link>
+                </h1>
+                <div>
+                  <Link to="/" className={styles['navitem']}>Home</Link>
+                  <Link to="/register" className={styles['navitem']}>Register Here</Link>
+                  <Link to="/login" className={styles['navitem']}>Log In</Link>
+                </div>
+              </div>
+            </div>
+        );
+    }
 }
 
 export default connect()(Header);
